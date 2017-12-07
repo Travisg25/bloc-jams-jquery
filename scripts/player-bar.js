ad28fd0866c1f0470c7371e740737ead68986664
Travis Grossman <TravisGrossman@Traviss-iMac.local>
2017-12-06 17:21:38 -0800

    checkpoint 14 complete

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -5,10 +5,6 @@
     $(this).attr('playState', player.playState);
   });
 
-  // $('button#next').on('click',  function() {
-  // (player.playState !== 'playing') ? return; : '';
-  // });
-
   $('button#next').on('click', function() {
      if (player.playState !== 'playing') { return; }
 
@@ -22,6 +18,7 @@
      player.playPause(nextSong);
 
   });
+
   $('button#previous').on('click', function() {
      if (player.playState !== 'playing') { return; }
 
@@ -29,7 +26,6 @@
      const previousSongIndex = currentSongIndex - 1 ;
 
      if (currentSongIndex == 0) { return; }
-     // if (previousSongIndex === undefined) { return; }
 
 
      const previousSong = album.songs[previousSongIndex];
@@ -37,5 +33,30 @@
 
   });
 
+  $('#time-control input').on('input', function (event){
+    player.skipTo(event.target.value);
+  });
 
+  setInterval (function () {
+    if(player.playState !== 'playing'){return;}
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control input').val(percent);
+    $('#time-current input').text(currentTime);
+
+    }, 1000);
+
+  // setInterval (function () {
+  //   if(player.playState !== 'playing'){return;}
+  //   const currentVolume = player.getTime();
+  //   const duration = player.getVolume();
+  //   const percent = (currentTime / duration) * 100;
+  //   }, 1000);
+
+
+
+  // setInterval( () => {
+  //      if (player.playState !== 'playing') { return; }
+  //    }, 1000);
 }
