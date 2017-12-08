1cffa08594f26049a807d4a787ff445bfdbbfc11
Travis Grossman <TravisGrossman@Traviss-iMac.local>
2017-12-08 13:21:35 -0800

    assignment 14 2/3 complete no Extra Credit complete

--- scripts/helper.js
+++ scripts/helper.js
@@ -3,7 +3,7 @@ class Helper {
     player.playPause(song)
     // update times
     // tims is probably in the song obj
-    console.log(song)
+    // console.log(song.duration)
     console.log(player)
     player.getDuration();
   }
